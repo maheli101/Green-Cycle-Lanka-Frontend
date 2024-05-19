facaae82b20dfd535079c40437026069d23f8d73
MahimiPansilu <ajithendri2000311perera@gmail.com>
2024-05-19 16:36:45 +0530

    add button

--- src/pages/Supplierpage/Supplier.jsx
+++ src/pages/Supplierpage/Supplier.jsx
@@ -1,11 +1,26 @@
 import React from 'react'
+import { Button } from 'react-bootstrap'
+
 
 function Supplier() {
   return (
     <div>
+      <br/>
+      
       <h1>
         Supplier page
       </h1>
+      <br/>
+      <Button variant="primary">Primary</Button>{' '}
+      <div className="d-grid gap-2">
+        <br/>
+      <Button variant="primary" size="lg">
+        Block level button
+      </Button>
+      <Button variant="secondary" size="lg">
+        Block level button
+      </Button>
+    </div>
     </div>
   )
 }
