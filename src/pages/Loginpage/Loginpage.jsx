a3ee5e120d72bb1e65df11067dab3f9a275bd47d
lakvindu2002 <lakvindusampath125@gmail.com>
2024-05-19 16:32:58 +0530

    Button added to home page

--- src/pages/Loginpage/Loginpage.jsx
+++ src/pages/Loginpage/Loginpage.jsx
@@ -1,12 +1,20 @@
-import React from 'react'
+//import React from 'react'
+import { Col, Container, Row } from 'react-bootstrap'
 
 function Loginpage() {
   return (
-    <div>
-      <h1>
-        Login page
-      </h1>
-    </div>
+    <>
+        <Container fluid>
+         <Row>
+          <Col lg={6} md={4} sm={12}>
+            <img src={} alt="" />
+          </Col>
+          <Col>laka2</Col>
+        </Row>
+    </Container>
+    
+    </>
+
   )
 }
 
