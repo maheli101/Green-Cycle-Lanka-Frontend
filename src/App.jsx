9dac60388b86b93e97c58f938b00045513edcf78
maheli101 <maheligeorge@gmail.com>
2024-05-24 19:42:53 +0530

    functioning home page, selection pages and about us page

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,15 @@
-
 import Dashboard from "./pages/Dashboard/Dashboard";
 import Loginpage from "./pages/Loginpage/Loginpage";
 import MainLayout from "./Layout/MainLayout/MainLayout";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Supplier from "./pages/Supplierpage/Supplier";
 
-import Register from "./pages/Register/Register"
+import Register from "./pages/Register/Register";
 import Vehicleregister from "./pages/Vehicleregister/Vehicleregister";
-import User from './pages/userProfile/userprofile'
+import User from "./pages/userProfile/userprofile";
 
-import BuyerSelectionPage from './pages/Buyer/BuyerSelectionPage';
-
+import BuyerSelectionPage from "./pages/Buyer/BuyerSelectionPage";
+import AboutUsPage from "./pages/AboutUs/AboutUsPage";
 
 function App() {
   return (
@@ -20,16 +19,14 @@ function App() {
           <Route path="home" element={<Dashboard />} />
           <Route path="supplier" element={<Supplier />} />
 
-          <Route path="user" element={<User/>} />
-         
+          <Route path="user" element={<User />} />
 
           <Route path="buyer" element={<BuyerSelectionPage />} />
-
+          <Route path="aboutus" element={<AboutUsPage />} />
         </Route>
         <Route path="login" element={<Loginpage />} />
         <Route path="register" element={<Register />} />
         <Route path="vehicle" element={<Vehicleregister />} />
-        
       </Routes>
     </Router>
   );
