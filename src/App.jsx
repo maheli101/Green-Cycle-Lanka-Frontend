3646502fae8ecf8e1c0ed137a163c50c9aa63899
DESKTOP-K6IHCUD\Sahan_Kumarage <kavishkasahandj@gmail.com>
2024-06-03 18:14:59 +0530

    updated the api

--- src/App.jsx
+++ src/App.jsx
@@ -3,32 +3,24 @@ import Loginpage from "./pages/Loginpage/Loginpage";
 import MainLayout from "./Layout/MainLayout/MainLayout";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Pickup from "./pages/Pickup/Main-pickup";
-import Start from "./pages/Start_trip/Start-trip"
-import { LoadScript } from '@react-google-maps/api';
+import Start from "./pages/Start_trip/Start-trip";
+import { LoadScript } from "@react-google-maps/api";
 import { Star } from "@mui/icons-material";
 import Welcome from "./pages/welcome-driver/welcome-driver";
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
-
-
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 
 import Supplier from "./pages/Supplierpage/Supplier";
-
-
-
 
 import PickupStatus from "./pages/Supplierpage/PickupStatus";
 import Selectitem from "./pages/Supplierpage/Selectitem";
 import Paper from "./pages/Supplierpage/Paper";
-
-
-
 
 import YardHomePage from "./pages/Yard/YardHomePage";
 import Pick from "./pages/Yard/Pick";
 import Order from "./pages/Yard/Order";
 import Updates from "./pages/Yard/Updates";
 
-import Register from "./pages/Register/Register"
+import Register from "./pages/Register/Register";
 
 import Vehicleregister from "./pages/Vehicleregister/Vehicleregister";
 import User from "./pages/userProfile/userprofile";
@@ -36,68 +28,40 @@ import User from "./pages/userProfile/userprofile";
 import BuyerSelectionPage from "./pages/Buyer/BuyerSelectionPage";
 import AboutUsPage from "./pages/AboutUs/AboutUsPage";
 
-
 function App() {
   return (
-    
-    
     <Router>
       <Routes>
         <Route path="/" element={<MainLayout />}>
           <Route path="home" element={<Dashboard />} />
 
-          <Route path="Pickup" element={<Pickup />}/>
-          <Route path="Start" element={<Start />}/>
-          <Route path="Welcome" element={<Welcome />}/>
+          <Route path="Pickup" element={<Pickup />} />
+          <Route path="Start" element={<Start />} />
+          <Route path="Welcome" element={<Welcome />} />
 
-
-        
-
-
-         
-
-          
-        
           <Route path="pickup" element={<PickupStatus />} />
           <Route path="Plastic" element={<Selectitem />} />
           <Route path="Paper" element={<Selectitem />} />
 
-
-       
-
-
           <Route path="supplier" element={<Supplier />} />
-
-
-       
-
-
 
           <Route path="yard" element={<YardHomePage />} />
           <Route path="pick" element={<Pick />} />
           <Route path="order" element={<Order />} />
           <Route path="update" element={<Updates />} />
 
-
           <Route path="user" element={<User />} />
 
           <Route path="buyer" element={<BuyerSelectionPage />} />
 
           <Route path="aboutus" element={<AboutUsPage />} />
-
-
-
-
-
         </Route>
         <Route path="login" element={<Loginpage />} />
         <Route path="register" element={<Register />} />
         <Route path="vehicle" element={<Vehicleregister />} />
       </Routes>
     </Router>
-  
   );
 }
 
 export default App;
-
--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -1,21 +1,33 @@
+import React, { useState } from "react";
 import { Col, Row } from "react-bootstrap";
 import "./Register.css";
 import registerBCK from "../../assets/registerphotos/Regback.jpeg";
-import { Link } from "react-router-dom";
+import { Link, json } from "react-router-dom";
 import { Formik, Form, Field, ErrorMessage } from "formik";
-import { RegisterValidation } from './RegisterValidation';
+import { RegisterValidation } from "./RegisterValidation";
+import axios from 'axios';
+import {post} from '../../Api/Axios.js'
+
+
 
 const initialValues = {
-  Name: "",
-  Email: "",
-  Number: "",
-  IDcard: "",
-  Pwd: "",
-  CPwd: "",
-  Verify: ""
+  name: "djdjd",
+  email: "samhghple@gmail.com",
+  contactNumber: "125453445444",
+  NIC: "566455565655455",
+  password: "123456",
+  type: "Driver",
 };
 
 export default function Register() {
+  const [response, setResponse] = useState('No response yet');
+
+const submit = async () => {
+     const response = await post('http://localhost:8000/User/postUser',initialValues )
+     console.log(response); 
+     setResponse(response);   
+  };
+
   return (
     <div>
       <Row style={{ height: "100vh" }}>
@@ -39,12 +51,9 @@ export default function Register() {
           <Formik
             initialValues={initialValues}
             validationSchema={RegisterValidation}
-            onSubmit={(values) => {
-              console.log(values);
-          
-            }}
+            // onSubmit={handleSubmit}
           >
-            {({ errors, touched }) => (
+            {({ errors, touched, handleChange, values }) => (
               <Form className="custom-reg-full">
                 <div className="custom-reg-allparts">
                   <div className="custom-reg-head">
@@ -58,70 +67,113 @@ export default function Register() {
                       type="text"
                       placeholder="Enter Name"
                       name="Name"
+                      value={values.Name}
+                      onChange={handleChange}
                       className={touched.Name && errors.Name ? "error" : ""}
                     />
-                    <ErrorMessage name="Name" component="small" className="error-message" />
+                    <ErrorMessage
+                      name="Name"
+                      component="small"
+                      className="error-message"
+                    />
 
                     <Field
                       type="text"
                       placeholder="Enter Email"
                       name="Email"
+                      value={values.Email}
+                      onChange={handleChange}
                       className={touched.Email && errors.Email ? "error" : ""}
                     />
-                    <ErrorMessage name="Email" component="small" className="error-message" />
+                    <ErrorMessage
+                      name="Email"
+                      component="small"
+                      className="error-message"
+                    />
 
                     <Field
                       type="text"
                       placeholder="Contact Number"
                       name="Number"
+                      value={values.Number}
+                      onChange={handleChange}
                       className={touched.Number && errors.Number ? "error" : ""}
                     />
-                    <ErrorMessage name="Number" component="small" className="error-message" />
+                    <ErrorMessage
+                      name="Number"
+                      component="small"
+                      className="error-message"
+                    />
 
                     <Field
                       type="text"
                       placeholder="NIC"
                       name="IDcard"
+                      value={values.IDcard}
+                      onChange={handleChange}
                       className={touched.IDcard && errors.IDcard ? "error" : ""}
                     />
-                    <ErrorMessage name="IDcard" component="small" className="error-message" />
+                    <ErrorMessage
+                      name="IDcard"
+                      component="small"
+                      className="error-message"
+                    />
 
                     <Field
                       type="password"
                       placeholder="Password"
                       name="Pwd"
+                      value={values.Pwd}
+                      onChange={handleChange}
                       className={touched.Pwd && errors.Pwd ? "error" : ""}
                     />
-                    <ErrorMessage name="Pwd" component="small" className="error-message" />
+                    <ErrorMessage
+                      name="Pwd"
+                      component="small"
+                      className="error-message"
+                    />
 
                     <Field
                       type="password"
                       placeholder="Repeat Password"
                       name="CPwd"
+                      value={values.CPwd}
+                      onChange={handleChange}
                       className={touched.CPwd && errors.CPwd ? "error" : ""}
                     />
-                    <ErrorMessage name="CPwd" component="small" className="error-message" />
+                    <ErrorMessage
+                      name="CPwd"
+                      component="small"
+                      className="error-message"
+                    />
                   </div>
 
                   <div className="custom-reg-dropdown">
                     <label htmlFor="userType">Choose Your Type:</label>
-                    <Field as="select" id="userType" name="userType">
+                    <Field
+                      as="select"
+                      id="userType"
+                      name="userType"
+                      value={values.userType}
+                      onChange={handleChange}
+                    >
                       <option value="driver">Driver</option>
                       <option value="buyer">User</option>
                     </Field>
                   </div>
-                  
-                  
+
                   <div className="custom-reg-lastf">
-                   <Link to="/login">
-                     <button type="submit" className="custom-reg-reg-btn">
+                    <button
+                      onClick={submit}
+                      type="submit"
+                      className="custom-reg-reg-btn"
+                    >
                       Register
                     </button>
-
-                   </Link>
                     <div className="custom-reg-sign-reg">
                       <p>
-                        Already have an account? <Link to="/login">Sign in</Link>
+                        Already have an account?{" "}
+                        <Link to="/login">Sign in</Link>
                       </p>
                     </div>
                   </div>
