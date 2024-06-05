2b946b0dea6925ede6f0db0f5a56d8b304193e65
maheli101 <maheligeorge@gmail.com>
2024-06-05 15:02:18 +0530

    created order and pickup table with backend

--- src/pages/Yard/Pick.jsx
+++ src/pages/Yard/Pick.jsx
@@ -1,364 +1,48 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
 function Pick() {
+  const [request, setRequests] = useState([]);
+
+  useEffect(() => {
+    fetchRequests();
+  }, []);
+
+  const fetchRequests = async () => {
+    try {
+      const response = await axios.get('http://localhost:8000/Request/getRequests');
+      setRequests(response.data);
+    } catch (error) {
+      console.error('Error fetching orders:', error);
+    }
+  };
+
   return (
     <>
-      <div
-        style={{
-          maxWidth: "1200px",
-          margin: "0 auto",
-          padding: "20px",
-          marginTop: "80px",
-          backgroundColor: "#f2f2f2",
-        }}
-      >
-        <div
-          style={{
-            backgroundColor: "#752121",
-            width: "100%",
-            maxWidth: "400px",
-            margin: "20px auto",
-            textAlign: "center",
-            borderRadius: "10px",
-            color: "white",
-            justifyContent: "center",
-            alignItems: "center",
-            display: "flex",
-          }}
-        >
-          <h1>Pick-Up Requests</h1>
-        </div>
-        <table
-          style={{
-            width: "100%",
-            borderCollapse: "collapse",
-            marginBottom: "30px",
-          }}
-        >
+      <div>
+        <h2>Pick up Requests</h2>
+        <table>
           <thead>
             <tr>
-              <th
-                style={{
-                  padding: "12px",
-                  textAlign: "center",
-                  border: "2px solid white",
-                  backgroundColor: "#277521",
-                  color: "white",
-                  fontSize: "20px",
-                }}
-              >
-                Type
-              </th>
-              <th
-                style={{
-                  padding: "12px",
-                  textAlign: "center",
-                  border: "2px solid white",
-                  backgroundColor: "#277521",
-                  color: "white",
-                  fontSize: "20px",
-                }}
-              >
-                Amount
-              </th>
-              <th
-                style={{
-                  padding: "12px",
-                  textAlign: "center",
-                  border: "2px solid white",
-                  backgroundColor: "#277521",
-                  color: "white",
-                  fontSize: "20px",
-                }}
-              >
-                Location
-              </th>
-              <th
-                style={{
-                  padding: "12px",
-                  textAlign: "center",
-                  border: "2px solid white",
-                  backgroundColor: "#277521",
-                  color: "white",
-                  fontSize: "20px",
-                }}
-              ></th>
-              <th
-                style={{
-                  padding: "12px",
-                  textAlign: "center",
-                  border: "2px solid white",
-                  backgroundColor: "#277521",
-                  color: "white",
-                  fontSize: "20px",
-                }}
-              ></th>
+              <th>User</th>
+              <th>Material</th>
+              <th>Amount</th>
+              <th>Town</th>
+              
             </tr>
           </thead>
           <tbody>
-            <tr>
-              <td
-                style={{
-                  padding: "12px",
-                  textAlign: "center",
-                  border: "2px solid white",
-                  backgroundColor: "#abdf94",
-                  fontSize: "18px",
-                }}
-              >
-                Plastic
-              </td>
-              <td
-                style={{
-                  padding: "12px",
-                  textAlign: "center",
-                  border: "2px solid white",
-                  backgroundColor: "#abdf94",
-                  fontSize: "18px",
-                }}
-              >
-                10kg
-              </td>
-              <td
-                style={{
-                  padding: "12px",
-                  textAlign: "center",
-                  border: "2px solid white",
-                  backgroundColor: "#abdf94",
-                  fontSize: "18px",
-                }}
-              >
-                17/A Wadduwa, Kaluthara
-              </td>
-              <td
-                style={{
-                  padding: "12px",
-                  textAlign: "center",
-                  border: "2px solid white",
-                  backgroundColor: "#abdf94",
-                  fontSize: "18px",
-                }}
-              >
-                <button
-                  style={{
-                    backgroundColor: "#277521",
-                    color: "white",
-                    borderRadius: "5px",
-                    border: "none",
-                    padding: "8px 16px",
-                    cursor: "pointer",
-                    transition: "background-color 0.3s ease",
-                  }}
-                >
-                  Accept
-                </button>
-              </td>
-              <td
-                style={{
-                  padding: "12px",
-                  textAlign: "center",
-                  border: "2px solid white",
-                  backgroundColor: "#abdf94",
-                  fontSize: "18px",
-                }}
-              >
-                <button
-                  style={{
-                    backgroundColor: "#752121",
-                    color: "white",
-                    borderRadius: "5px",
-                    border: "none",
-                    padding: "8px 16px",
-                    cursor: "pointer",
-                    transition: "background-color 0.3s ease",
-                  }}
-                >
-                  Delete
-                </button>
-              </td>
-            </tr>
-            <tr>
-              <td
-                style={{
-                  padding: "12px",
-                  textAlign: "center",
-                  border: "2px solid white",
-                  backgroundColor: "#abdf94",
-                  fontSize: "18px",
-                }}
-              >
-                Paper
-              </td>
-              <td
-                style={{
-                  padding: "12px",
-                  textAlign: "center",
-                  border: "2px solid white",
-                  backgroundColor: "#abdf94",
-                  fontSize: "18px",
-                }}
-              >
-                100kg
-              </td>
-              <td
-                style={{
-                  padding: "12px",
-                  textAlign: "center",
-                  border: "2px solid white",
-                  backgroundColor: "#abdf94",
-                  fontSize: "18px",
-                }}
-              >
-                102 Kiribathgoda, Wawala
-              </td>
-              <td
-                style={{
-                  padding: "12px",
-                  textAlign: "center",
-                  border: "2px solid white",
-                  backgroundColor: "#abdf94",
-                  fontSize: "18px",
-                }}
-              >
-                <button
-                  style={{
-                    backgroundColor: "#277521",
-                    color: "white",
-                    borderRadius: "5px",
-                    border: "none",
-                    padding: "8px 16px",
-                    cursor: "pointer",
-                    transition: "background-color 0.3s ease",
-                  }}
-                >
-                  Accept
-                </button>
-              </td>
-              <td
-                style={{
-                  padding: "12px",
-                  textAlign: "center",
-                  border: "2px solid white",
-                  backgroundColor: "#abdf94",
-                  fontSize: "18px",
-                }}
-              >
-                <button
-                  style={{
-                    backgroundColor: "#752121",
-                    color: "white",
-                    borderRadius: "5px",
-                    border: "none",
-                    padding: "8px 16px",
-                    cursor: "pointer",
-                    transition: "background-color 0.3s ease",
-                  }}
-                >
-                  Delete
-                </button>
-              </td>
-            </tr>
-            <tr>
-              <td
-                style={{
-                  padding: "12px",
-                  textAlign: "center",
-                  border: "2px solid white",
-                  backgroundColor: "#abdf94",
-                  fontSize: "18px",
-                }}
-              >
-                Plastic
-              </td>
-              <td
-                style={{
-                  padding: "12px",
-                  textAlign: "center",
-                  border: "2px solid white",
-                  backgroundColor: "#abdf94",
-                  fontSize: "18px",
-                }}
-              >
-                5kg
-              </td>
-              <td
-                style={{
-                  padding: "12px",
-                  textAlign: "center",
-                  border: "2px solid white",
-                  backgroundColor: "#abdf94",
-                  fontSize: "18px",
-                }}
-              >
-                17/A Wadduwa, Kaluthara
-              </td>
-              <td
-                style={{
-                  padding: "12px",
-                  textAlign: "center",
-                  border: "2px solid white",
-                  backgroundColor: "#abdf94",
-                  fontSize: "18px",
-                }}
-              >
-                <button
-                  style={{
-                    backgroundColor: "#277521",
-                    color: "white",
-                    borderRadius: "5px",
-                    border: "none",
-                    padding: "8px 16px",
-                    cursor: "pointer",
-                    transition: "background-color 0.3s ease",
-                  }}
-                >
-                  Accept
-                </button>
-              </td>
-              <td
-                style={{
-                  padding: "12px",
-                  textAlign: "center",
-                  border: "2px solid white",
-                  backgroundColor: "#abdf94",
-                  fontSize: "18px",
-                }}
-              >
-                <button
-                  style={{
-                    backgroundColor: "#752121",
-                    color: "white",
-                    borderRadius: "5px",
-                    border: "none",
-                    padding: "8px 16px",
-                    cursor: "pointer",
-                    transition: "background-color 0.3s ease",
-                  }}
-                >
-                  Delete
-                </button>
-              </td>
-            </tr>
+            {request.map((request, index) => (
+              <tr key={index}>
+                <td>{request.user_id ? request.user_id.name : "N/A"}</td>
+                <td>{request.material}</td>
+                <td>{request.amount}</td>
+                <td>{request.town}</td>
+                
+              </tr>
+            ))}
           </tbody>
         </table>
-        <Link to="/yard">
-          <button
-            style={{
-              backgroundColor: "#752121",
-              color: "white",
-              width: "15%",
-              border: "none",
-              borderRadius: "5px",
-              padding: "8px 16px",
-              cursor: "pointer",
-              float: "right",
-              marginRight: "1%",
-              marginTop: "5%",
-            }}
-          >
-            Back
-          </button>
-        </Link>
       </div>
     </>
   );
