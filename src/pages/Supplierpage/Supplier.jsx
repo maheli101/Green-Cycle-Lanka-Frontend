eb4c2d60f0cb3aa6378cae37dc32667002f4250f
maheli101 <maheligeorge@gmail.com>
2024-06-06 11:22:14 +0530

    form validation

--- src/pages/Supplierpage/Supplier.jsx
+++ src/pages/Supplierpage/Supplier.jsx
@@ -8,6 +8,8 @@ import categoryImg2 from '../../assets/img/categoryImg2.jpg';
 import categoryImg3 from '../../assets/img/categoryImg3.jpg';
 import categoryImg4 from '../../assets/img/categoryImg4.jpg';
 
+import { Link } from "react-router-dom";
+
 function ContainerFluidExample() {
   const sectionStyle = {
     backgroundColor: "#d8e8d8", // Light sage green color
@@ -50,25 +52,33 @@ function ContainerFluidExample() {
 
       <Row className="mt-4 justify-content-center">
         <Col xs={6} sm={3} >
+        <Link to="/requestForm">
           <button style={buttonStyle} onClick={() => alert("Category 1 clicked!")}>
             <img src={categoryImg1} alt="Category 1" style={imageStyle} />
           </button>
+          </Link>
         </Col>
         <Col xs={6} sm={3} >
+        <Link to="/requestForm">
           <button style={buttonStyle} onClick={() => alert("Category 2 clicked!")}>
             <img src={categoryImg2} alt="Category 2" style={imageStyle} />
           </button>
+          </Link>
         </Col>
         <Col xs={6} sm={3} >
+        <Link to="/requestForm">
           <button style={buttonStyle} onClick={() => alert("Category 3 clicked!")}>
             <img src={categoryImg3} alt="Category 3" style={imageStyle} />
           </button>
+          </Link>
         </Col>
         <Col xs={6} sm={3} >
-          
+
+          <Link to="/requestForm">
           <button style={buttonStyle} onClick={() => alert("Category 4 clicked!")}>
             <img src={categoryImg4} alt="Category 4" style={imageStyle} />
           </button>
+          </Link>
           
         </Col>
       </Row>
